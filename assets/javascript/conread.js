3d5733b0cd5fc63e2f2097324631428799363c5f
bgreen53 <green.barry53@gmail.com>
2020-03-16 21:19:14 -0700

    adding API branch

---
+++ assets/javascript/conread.js
@@ -0,0 +1,14 @@
+
+//google maps API
+ async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqvGNseOhu5v3Yg0gHMVe4unXgRoLuPQw&callback=initMap"
+  
+
+ //Open weather API
+api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=b6bcb836bbf008c6cea4df94d9ca492c
+
+//Spotify API
+api.spotify.com
+
+//Ticketmaster API
+"https://app.ticketmaster.com/{package}/{version}/{resource}.json?apikey=b7YejLcyutp7MC8dbDuipAPR4yrvDGy7"
+

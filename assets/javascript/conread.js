e50a40aa205f3900a5fd66c66ca75ef535f2f0ab
bgreen53 <green.barry53@gmail.com>
2020-03-23 18:58:27 -0700

    styled show display area

--- assets/javascript/conread.js
+++ assets/javascript/conread.js
@@ -6,6 +6,8 @@
  //Open weather API
 //api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=b6bcb836bbf008c6cea4df94d9ca492c
 
+
+
 //Spotify API
 //api.spotify.com
 
@@ -16,6 +18,7 @@
 
 //Bands in Town
 //curl -X GET "https://rest.bandsintown.com/artists/A%20Day%20To%20Remember/events?app_id=642b1873e2de4bb01c82a203278b77e2" -H "accept: application/json"
+var results = []
 
 $(document).ready(function(){
     $('.datepicker').datepicker();
@@ -24,11 +27,12 @@ $(document).ready(function(){
 
 $(document).on("click", "#search", function(event) {
     event.preventDefault()
+    $("#info").empty()
     var band = $("#artist").val().trim().split(" ").join("%20")
     
     console.log(band)
     var queryURL = "https://rest.bandsintown.com/artists/"+ band +"/events?app_id=642b1873e2de4bb01c82a203278b77e2" 
-    //-H "accept: application/json"
+   
 
   console.log(band)
   console.log(queryURL)
@@ -38,14 +42,15 @@ $(document).on("click", "#search", function(event) {
       method: "GET"
     })
       .then(function(response) {
-       // var results = response.data;
+       results = response;
        console.log(response)
        console.log(response[0].venue.name)
        //console.log(results)
        console.log(response[0].artist.image_url)
-        
-        for (var i = 0; i < response.length; i++) {
+      
+        for (var i = 0; i < 6; i++) {
           var showInfo = $("<div>");
+          showInfo.attr("class", "card")
           showInfo.append(venues)
           showInfo.append(venueLoc)
           showInfo.append(date)
@@ -54,20 +59,29 @@ $(document).on("click", "#search", function(event) {
 
 
           var pic=$("<img>");
-          pic.attr("src", response[0].artist.thumb_url)
+          pic.attr("src", response[0].artist.image_url)
           var shows = response[i];
-          var venues= $("<p>").html(response[i].venue.name)
-          var venueLoc=$("<p>").html(response[i].venue.city + ", " + response[i].venue.region)
-          var date= $("<p>").html(response[i].datetime)
+          var venues= $("<p>").html("Venue: " + response[i].venue.name)
+          var venueLoc=$("<p>").html("City: " + response[i].venue.city + ", " + response[i].venue.region)
+          var date= $("<p>").html("When: " +response[i].datetime)
+          var artInfo=$("<p>").html(response[0].artist.name)
+          var link=$("<a>").html("<i class='fa fa-facebook-square fa-lg' aria-hidden='true'></i>")
+          link.attr("href", response[0].artist.facebook_page_url)
+          console.log(response[0].artist.facebook_page_url)
           
             console.log(pic)
 
             if(response[i].venue.region === ""){
-               venueLoc= $("<p>").html(response[i].venue.city + ", " + response[i].venue.country)
+               venueLoc= $("<p>").html("City: " +response[i].venue.city + ", " + response[i].venue.country)
               }
             
         $("#pic").html(pic)
         $("#info").append(showInfo)
+        $("#artist").empty()
+        $("#artInfo").html(artInfo)
+        $("#fbLink").html(link)
+        $("#fbLink").append("Like them on Facebook!")
+        
         //$("#info").append(venueLoc)
         //$("#info").append(date)
         }
@@ -81,6 +95,9 @@ $(document).on("click", "#search", function(event) {
 
          //$("#gifs").prepend(gifDiv);
         });
-      
-    
+ 
     });
+
+
+
+    
